ef8f67456e975fa02b6f0ab6a1a2f7ebf8b44c08
Sara Jacobson <drupal@notgoddess.com>
2019-10-22 14:44:30 -0500

    Updates

---
+++ precache-manifest.32969b6db8212c0ac60174e7ec706528.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d05ccd95253bfdee5adb67bb6f486e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c57b8549c1712e7a6ef",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1ca369ad97c7384084fd",
+    "url": "/static/js/2.fe8f784a.chunk.js"
+  },
+  {
+    "revision": "0c57b8549c1712e7a6ef",
+    "url": "/static/js/main.a8b269af.chunk.js"
+  },
+  {
+    "revision": "52b78c198a3cfbe35bc6",
+    "url": "/static/js/runtime-main.81ace809.js"
+  }
+]);
